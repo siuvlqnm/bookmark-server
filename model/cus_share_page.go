36679470ad9535f12f642fa184420b6c000a0be1
siuvlqnm <siuvlqnm@gmail.com>
2021-01-29 17:10:28 +0800

    分享页

---
+++ model/cus_share_page.go
@@ -0,0 +1,12 @@
+package model
+
+import "github.com/siuvlqnm/bookmark/global"
+
+type CusSharePage struct {
+	global.GVA_MODEL
+	CusUserID    uint   `json:"cusUserId"`
+	PSeaEngineID uint32 `json:"pSeaEngineId"`
+	PageName     string `json:"pageName"`
+	IsPassword   bool   `json:"isPassword"`
+	PagePassword string `json:"pagePassword"`
+}
